35e1ccbeb9acf31990d223a6bade8fdb45a7c48d
Oleksii Aleferchuk <alex.aleferchuk@gmail.com>
2024-12-17 16:37:00 +0000

    Add CRUD

--- secret_santa/urls.py
+++ secret_santa/urls.py
@@ -2,5 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.HomePage.as_view(), name='home'),
+    path("", views.EventList.as_view(), name="home"),
+    path("add_event/", views.add_event, name="add_event"),
+    path("delete_event/<int:event_id>/", views.delete_event, name="delete_event"),
+    path("edit_event/<int:edit_id>/", views.edit_event, name="edit_event"),
+    path("view_event/<int:event_id>/", views.view_event, name="view_event"),
 ]
